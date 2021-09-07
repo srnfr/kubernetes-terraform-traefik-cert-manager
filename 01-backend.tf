a953cdf46b97a26a0579252f624b0533f5838ee4
Stephane REYTAN <srnfr@users.noreply.github.com>
2021-09-07 18:33:06 +0200

    Update 01-backend.tf

--- 01-backend.tf
+++ 01-backend.tf
@@ -1,9 +1,11 @@
 terraform {
-  ##required_version = "~> 0.13.5"
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      ##version = "~> 2.0.2"
     }
   }
+}
+
+provider "digitalocean" {
+  token = var.do_token
 }
